2a8575ca0538ded9f72817228d702328f73ec56c
remyjck <remy.seassau@polytechnique.edu>
2022-01-19 17:35:34 +0100

    Extended vertex type, added dfs and removed commented code

--- lib/graph.ml
+++ lib/graph.ml
@@ -6,31 +6,37 @@ type atom =
     pol : bool;
   }
 
-type edge_list_graph =
+type visited = 
+  | Unvisited
+  | Visited
+
+type vertex = 
   {
-    nodes : atom list;
-    edges : (atom * atom) list;
+    value : atom;
+    mutable visited : visited;
+    mutable alpha : vertex list;
   }
 
-module AtomMap = Map.Make(struct type t = atom let compare = compare end);;
+type edge_list_graph =
+  {
+    nodes : vertex list;
+    edges : (vertex * vertex) list;
+  }
+
+module AtomMap = Map.Make(struct type t = vertex let compare = compare end);;
 
 type map_graph = 
   {
-    nodes : atom list;
-    edges : atom list AtomMap.t;
+    nodes : vertex list;
+    edges : vertex list AtomMap.t;
   }
-
-let add_or_init new_elem y = 
-  match y with
-  | None -> Some [new_elem]
-  | Some z -> Some (new_elem :: z)
 
 let rec edge_list_to_map edges atommap = 
   match edges with
   | [] -> atommap
   | (x, y) :: t -> 
-    let atommap1 = AtomMap.update x (add_or_init y) atommap in
-    let atommap2 = AtomMap.update y (add_or_init y) atommap1 in
+    let atommap1 = AtomMap.update x (Util.add_or_init y) atommap in
+    let atommap2 = AtomMap.update y (Util.add_or_init y) atommap1 in
     edge_list_to_map t atommap2
 
 let edge_list_to_map (g : edge_list_graph) = 
@@ -38,19 +44,6 @@ let edge_list_to_map (g : edge_list_graph) =
     nodes = g.nodes;
     edges = edge_list_to_map g.edges AtomMap.empty
   }
-
-(* let rec induced_graph_edges edges vertices res = 
-  match edges with
-  | [] -> res 
-  | (x, y) :: t -> 
-    if (List.mem x vertices) && (List.mem y vertices) then
-      induced_graph_edges t vertices ((x, y) :: res)
-    else 
-      induced_graph_edges t vertices res
-
-let induced_graph (g : edge_list_graph) vertices : edge_list_graph =
-  let induced_edges = induced_graph_edges g.edges vertices [] in
-  {nodes = vertices; edges = induced_edges};; *)
 
 let induced_graph_edges edges vertices = 
   AtomMap.map 
@@ -66,16 +59,6 @@ let induced_graph g vertices =
     edges = induced_edges
   }
 
-(* let rec neighbours edges x acc =
-  match edges with
-  | [] -> acc
-  | (y, z) :: t when (z = x) -> neighbours t x (y :: acc)
-  | (z, y) :: t when (z = x) -> neighbours t x (y :: acc)
-  | _ :: t -> neighbours t x acc
-
-let neighbours (g : edge_list_graph) x = 
-  neighbours g.edges x [];; *)
-
 let neighbours g x = 
   let neighb = AtomMap.find_opt x g.edges in 
   match neighb with
@@ -90,21 +73,6 @@ let compute_neighborhoods g x =
   let nnbgh = non_neighborhood g neighborhood in
   (neighborhood, nnbgh)
 
-(* let rec is_edge edges (x, y) =
-  match edges with
-  | [] -> false
-  | (i, j) :: t -> if (i = x && j = y) || (i = y && j = x) then true else is_edge t (x, y)
-
-let rec neighbour_list edges x neighb = 
-  match neighb with 
-  | [] -> false
-  | y :: t -> if is_edge edges (x, y) then true else neighbour_list edges x t
-
-let rec neighbours_list (g : edge_list_graph) n nl na =
-  match nl with
-  | [] -> na
-  | x :: t -> if neighbour_list g.edges x n then neighbours_list g n nl (x :: na) else neighbours_list g n t na *)
-
 let rec neighbour_list g neigh nngh res = 
   match nngh with 
   | [] -> res
@@ -115,3 +83,26 @@ let rec neighbour_list g neigh nngh res =
 
 let neighbour_list g neighx nnghx = 
   neighbour_list g neighx nnghx []
+
+let dfs graph =
+  match graph.nodes with
+  | [] -> []
+  | start :: _ ->
+    let rec rdfs visited node =
+      if not (List.mem node visited) then
+        begin
+          let s = neighbours graph node in
+          List.fold_left rdfs (node :: visited) s
+        end
+      else visited
+    in rdfs [] start
+
+let is_connected graph =
+  let visited = dfs graph in
+  List.length graph.nodes = List.length visited
+
+let complement graph = 
+  let compl_edges = AtomMap.map 
+    (fun vl -> Util.list_difference graph.nodes vl)
+    graph.edges
+  in { nodes = graph.nodes; edges = compl_edges}
--- lib/graph.mli
+++ lib/graph.mli
@@ -7,31 +7,48 @@ type atom =
       pol : bool;
   }
 
-type edge_list_graph = 
+type visited = 
+  | Unvisited
+  | Visited
+
+type vertex = 
   {
-      nodes : atom list;
-      edges : (atom * atom) list;
+    value : atom;
+    mutable visited : visited;
+    mutable alpha : vertex list;
   }
 
-module AtomMap : Map.S with type key = atom
+type edge_list_graph = 
+  {
+      nodes : vertex list;
+      edges : (vertex * vertex) list;
+  }
+
+module AtomMap : Map.S with type key = vertex
 
 type map_graph = 
   {
-      nodes : atom list;
-      edges : atom list AtomMap.t;
+      nodes : vertex list;
+      edges : vertex list AtomMap.t;
   }
 
 val edge_list_to_map : edge_list_graph -> map_graph
 
-val induced_graph : map_graph -> atom list -> map_graph
+val induced_graph : map_graph -> vertex list -> map_graph
 (** [induced_graph graph vertices] returns the graph induced by [vertices] *)
 
-val neighbours : map_graph -> atom -> atom list
+val neighbours : map_graph -> vertex -> vertex list
 
-val compute_neighborhoods : map_graph -> atom -> atom list * atom list
+val compute_neighborhoods : map_graph -> vertex -> vertex list * vertex list
 (** [compute_neighborhoods graph vertex] returns a tuple containing
     the neighborhood and non-neighborhood of the vertex *)
 
-val neighbour_list : map_graph -> atom list -> atom list -> atom list
+val neighbour_list : map_graph -> vertex list -> vertex list -> vertex list
 (** [neighbour_list graph neighbour_list list] returns the sublist of [list] 
     where the elements have at least one neighbour in [neighbour] *)
+
+val dfs : map_graph -> vertex list
+
+val is_connected : map_graph -> bool
+
+val complement : map_graph -> map_graph
