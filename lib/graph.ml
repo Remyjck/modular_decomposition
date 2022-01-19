ee8188c5606ba538d53b5bcefae1268586f0d265
remyjck <remy.seassau@polytechnique.edu>
2022-01-19 01:44:39 +0100

    Updated utility functions to work on map_graph

--- lib/graph.ml
+++ lib/graph.ml
@@ -6,13 +6,13 @@ type atom =
     pol : bool;
   }
 
-module AtomMap = Map.Make(struct type t = atom let compare = compare end);;
-
 type edge_list_graph =
   {
     nodes : atom list;
     edges : (atom * atom) list;
   }
+
+module AtomMap = Map.Make(struct type t = atom let compare = compare end);;
 
 type map_graph = 
   {
@@ -39,7 +39,7 @@ let edge_list_to_hashmap (g : edge_list_graph) =
     edges = edge_list_to_hashmap g.edges AtomMap.empty
   }
 
-let rec induced_graph_edges edges vertices res = 
+(* let rec induced_graph_edges edges vertices res = 
   match edges with
   | [] -> res 
   | (x, y) :: t -> 
@@ -50,9 +50,23 @@ let rec induced_graph_edges edges vertices res =
 
 let induced_graph (g : edge_list_graph) vertices : edge_list_graph =
   let induced_edges = induced_graph_edges g.edges vertices [] in
-  {nodes = vertices; edges = induced_edges};;
+  {nodes = vertices; edges = induced_edges};; *)
 
-let rec neighbours edges x acc =
+let induced_graph_edges edges vertices = 
+  AtomMap.map 
+    (fun vl -> List.filter 
+      (fun x -> (List.mem x vertices))
+      vl)
+    edges
+
+let induced_graph g vertices = 
+  let induced_edges = induced_graph_edges g.edges vertices in 
+  { 
+    nodes = vertices;
+    edges = induced_edges
+  }
+
+(* let rec neighbours edges x acc =
   match edges with
   | [] -> acc
   | (y, z) :: t when (z = x) -> neighbours t x (y :: acc)
@@ -60,17 +74,23 @@ let rec neighbours edges x acc =
   | _ :: t -> neighbours t x acc
 
 let neighbours (g : edge_list_graph) x = 
-  neighbours g.edges x [];;
+  neighbours g.edges x [];; *)
 
-let non_neighborhood (g : edge_list_graph) neighbours =
-  List.filter (fun x -> not (List.mem x neighbours)) g.nodes;;
+let neighbours g x = 
+  let neighb = AtomMap.find_opt x g.edges in 
+  match neighb with
+  | None -> []
+  | Some l -> l
+
+let non_neighborhood g neighbours =
+  List.filter (fun x -> not (List.mem x neighbours)) g.nodes
 
 let compute_neighborhoods g x =
   let neighborhood = neighbours g x in
   let nnbgh = non_neighborhood g neighborhood in
-  (neighborhood, nnbgh);;
+  (neighborhood, nnbgh)
 
-let rec is_edge edges (x, y) =
+(* let rec is_edge edges (x, y) =
   match edges with
   | [] -> false
   | (i, j) :: t -> if (i = x && j = y) || (i = y && j = x) then true else is_edge t (x, y)
@@ -83,4 +103,15 @@ let rec neighbour_list edges x neighb =
 let rec neighbours_list (g : edge_list_graph) n nl na =
   match nl with
   | [] -> na
-  | x :: t -> if neighbour_list g.edges x n then neighbours_list g n nl (x :: na) else neighbours_list g n t na
+  | x :: t -> if neighbour_list g.edges x n then neighbours_list g n nl (x :: na) else neighbours_list g n t na *)
+
+let rec neighbour_list g neigh nngh res = 
+  match nngh with 
+  | [] -> res
+  | x :: t -> let neighx = neighbours g x in
+    match List.find_opt (fun x -> List.mem x neigh) neighx with
+    | None -> neighbour_list g neigh t res
+    | Some _ -> neighbour_list g neigh t (x :: res)
+
+let neighbour_list g neighx nnghx = 
+  neighbour_list g neighx nnghx []
