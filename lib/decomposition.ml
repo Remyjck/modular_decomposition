562fab7e287502a367d3a7d21a41d892c6268273
remyjck <remy.seassau@polytechnique.edu>
2022-02-02 17:02:06 +0100

    Started refine_top, still need to finish marking

--- lib/decomposition.ml
+++ lib/decomposition.ml
@@ -16,12 +16,14 @@ let partition p tj =
     (fun t -> List.mem t.id t_ids)
     p.tree.successors
 
-let refine forest x direction = 
+(* TODO: direction only indicates the placement of v relative to x,
+   still need to check placement of the tree being refined *)
+let refine forest alpha direction = 
   let (mark : marking) = match direction with 
     | Left -> Left
     | Right -> Right
   in
-  let max_subtrees = maximal_subtrees forest x in
+  let max_subtrees = maximal_subtrees forest alpha in
   let parent_nodes = parents max_subtrees in
   let rec refine_iter parent_list forest =
     match parent_list with
@@ -50,6 +52,44 @@ let refine forest x direction =
       refine_iter t new_forest
   in
   refine_iter parent_nodes forest
+
+let rec position v x forest =
+  match forest with
+  | [] -> failwith "Neither vertex nor x found"
+  | h :: t ->
+    let nodes = tree_nodes h.tree in
+    if List.mem v nodes then
+      Left
+    else if List.mem x nodes then
+      Right
+    else
+      position v x t
+
+let active_edges graph v forest x =
+  let rec siblings v forest =
+    match forest with
+    | [] -> failwith "Parent of v not found"
+    | h :: t ->
+      let potential_siblings = tree_nodes h.tree in
+      if List.mem v potential_siblings then
+        List.filter (fun n -> v <> n && v <> x)  potential_siblings
+      else
+        siblings v t
+  in 
+  let siblings = siblings v forest in
+  let neighbours = Graph.AtomMap.find {value = v; visited = Unvisited} graph in
+  List.filter (fun v -> List.mem v.Graph.value siblings |> not) neighbours
+        
+let rec refine_top graph vertices x forest =
+  match vertices with
+  | [] -> forest
+  | v :: t ->
+    let direction = position v x forest in
+    let active_edges = active_edges graph v forest x
+      |> List.map (fun v -> v.Graph.value) 
+    in
+    let new_forest = refine forest active_edges direction in
+    refine_top graph t x new_forest
 
 let promotion forest =
   let left_promoted = unfold forest Left in
