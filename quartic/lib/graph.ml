3ff28f3c179e6c0dd0912232899e3fea749670c0
remyjck <remy.seassau@polytechnique.edu>
2022-02-04 19:25:27 +0100

    Added most helper functions (only algorithm 3.6 from the paper remains)

--- quartic/lib/graph.ml
+++ quartic/lib/graph.ml
@@ -6,9 +6,18 @@ type atom =
     pol : bool;
   }
 
+module IMap = Map.Make(Int)
+module ISet = Set.Make(Int)
+
+type node =
+  | Atom of atom
+  | Tensor
+  | Par
+  | Prime of ISet.t IMap.t
+
 type vertex = 
   {
-    value : atom;
+    connective : node;
     id : int;
   }
 
@@ -26,6 +35,7 @@ type graph =
     edges : VSet.t VMap.t;
   }
 
+(** [<~> graph vertices]: subgraph of [graph] that contains all vertices not in [vertices] *)
 let (<~>) graph vertices =
   let nodes = VSet.diff graph.nodes vertices in
   let edges =
@@ -33,3 +43,111 @@ let (<~>) graph vertices =
     |> VMap.map (fun s -> VSet.diff s vertices)
   in
   {nodes = nodes; edges = edges}
+
+(** [induced_subgraph graph vertices]: subgraph of [graph] that contains only the vertices in [vertices]  *)
+let induced_subgraph graph vertices =
+  let edges = 
+    VMap.filter (fun v _ -> (VSet.mem v vertices)) graph.edges
+    |> VMap.map (fun s -> VSet.inter s vertices)
+  in
+  {nodes = vertices; edges = edges}
+
+(** [connected graph vertices]: set of vertices to which [vertices] is connected *)
+let connected graph vertices =
+    VSet.fold (fun v -> VMap.find v graph |> VSet.union) vertices VSet.empty
+
+(** [w G H h]: set of vertices of [G <~> H] to which [h] is connected *)
+let w g h v =
+  VMap.find v (g <~> h).edges 
+  
+(** [is_module G H]: checks if [H] is a module of [G] *)  
+let is_module g h =
+  let connected = VSet.choose h |> w g h in
+  VSet.for_all (fun v -> connected = w g h v)
+  
+(** [smallest_condensible graph set]: returns the smallest condensible set containing all vertices of [set] *)
+let smallest_condensible g v =
+  let vl = VSet.elements v in
+  let rec smallest_condesible wl i b =
+    if i > List.length wl then
+      VSet.of_list wl
+    else
+      let wi = List.nth wl (i-1) in
+      let connected = w g (VSet.of_list wl) wi in
+      let new_b = VSet.union connected b in
+      let to_add = 
+        VSet.diff (new_b) (VSet.inter b connected)
+        |> VSet.elements
+      in
+      let new_wl = List.append wl to_add in
+      smallest_condesible new_wl (i+1) new_b
+  in
+  let b = w g v (List.hd vl) in
+  smallest_condesible vl 2 b
+
+type subset =
+  | Clique of VSet.t 
+  | IndSet of VSet.t
+
+module Subset = struct
+  type t = subset
+  let compare s1 s2 =
+    match s1, s2 with
+    | Clique ss1, Clique ss2 -> VSet.compare ss1 ss2
+    | IndSet ss1, IndSet ss2 -> VSet.compare ss1 ss2
+    | Clique _, IndSet _ -> 1
+    | IndSet _, Clique _ -> 0
+end
+
+module Subsetset = Set.Make(Subset)
+
+let update_subset subset vi vi_neighbours vj vj_neighbours =
+  match subset with
+  | IndSet set -> 
+    if vj_neighbours = vi_neighbours then
+      IndSet (VSet.add vj set)
+    else
+      subset
+  | Clique set ->
+    if vj_neighbours = (VSet.add vi vi_neighbours) then
+      Clique (VSet.add vj set)
+    else if (VSet.cardinal set = 1) && (vj_neighbours = vi_neighbours) then
+      IndSet (VSet.add vj set)
+    else
+      subset
+
+(** [update_subsetset subsetset new_subset]: given a set of subsets [subsetset], update it by adding [new_subset] *)
+let update_subsetset subsetset new_subset =
+  match new_subset with
+  | IndSet _ -> Subsetset.add new_subset subsetset
+  | Clique set ->
+    if VSet.cardinal set = 1 then
+      subsetset
+    else 
+      (Subsetset.add new_subset subsetset)
+
+(** [cc_and_is graph]: returns the set of maximal condensible cliques and independent set of [graph] *)
+let cc_and_is g =
+  let len_v = (VSet.cardinal g.nodes) in
+  let v = VSet.elements g.nodes in
+  let rec iterate_i i res =
+    if i >= len_v then
+      res
+    else
+      let vi = List.nth v (i-1) in
+      let vi_neighbours = VMap.find vi g.edges in
+      let rec iterate_j j subset =
+        if j >= len_v then
+          subset
+        else
+          let vj = List.nth v (j-1) in
+          let vj_neighbours = VMap.find vj g.edges in
+          let updated_subset = update_subset subset vi vi_neighbours vj vj_neighbours in
+          iterate_j (j+1) updated_subset
+      in
+      let j = i+1 in
+      let new_subset = iterate_j j (Clique (VSet.singleton vi)) in
+      let updated_res = update_subsetset res new_subset in
+      iterate_i (i+1) updated_res
+  in
+  iterate_i 1 (Subsetset.empty)
