4a22752f3cf488746ca2dfc20ef5de96b405aab5
Gwentinho <qschroed@gmail.com>
2023-02-19 16:26:26 +0100

    renamed the w function

--- lib/graph_module.mli
+++ lib/graph_module.mli
@@ -1,2 +1,17 @@
-val share_module : Graph.graph -> Graph.vertex -> Graph.vertex -> bool
+type context = {
+  graph : Graph.graph;
+  connnected : Graph.verticies;
+}
+
+val share_module :
+  Graph.graph -> Graph.vertex -> Graph.vertex -> bool
+
 val is_module : Graph.graph -> Graph.verticies -> bool
+
+val create_context :
+  Graph.graph -> Graph.verticies -> context option
+
+val apply_context: context -> Graph.graph -> Graph.graph
+
+(** keep_links g h v, finds all the nodes connected to v in g - h*)
+val keep_links: Graph.graph -> Graph.verticies -> Graph.vertex -> Graph.verticies
