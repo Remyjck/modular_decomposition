a9cab6d13792f2c67ec99dca66aba439697b4c3c
Gwentinho <qschroed@gmail.com>
2023-03-13 22:33:21 +0100

    removed redundant context code

--- lib/graph_module.ml
+++ lib/graph_module.ml
@@ -25,29 +25,3 @@ let is_module g (h:verticies) =
     ~f:(fun v ->
       let v_connected = keep_links g h v in
       Set.equal connected v_connected)
-
-
-
-
-let create_context graph modu =
-  if is_module graph modu then (*remove incident edges or smth*)
-    let remGraph = graph_difference graph modu in
-    let singleVertexOpt = Set.find modu ~f:(fun _ -> true) in
-    match singleVertexOpt with
-    | None -> None
-    | Some v ->
-      let r = keep_links graph modu v in
-      Some {graph= remGraph; connnected= r}
-  else None
-
-(*take context and graph and make the new graph boom done*)
-(*
-let apply_context context graph =
-  let new_nodes = Set.union graph.nodes context.graph.nodes in
-  let basic_edges = Map.merge graph.edges context.graph.edges ~f:(*TODO*) in
-  let new_edges = connect_vertices context.connnected graph.nodes (empty_graph ()) in
-  let new_edges = Map.merge new_edges.edges basic_edges  in
-  {nodes=new_nodes; edges=new_edges}
-   *)
-
-
--- lib/graph_module.mli
+++ lib/graph_module.mli
@@ -8,14 +8,5 @@ val share_module :
 
 val is_module : Graph.graph -> Graph.verticies -> bool
 
-val create_context :
-  Graph.graph -> Graph.verticies -> context option
-
-  (*
-
-  val apply_context: context -> Graph.Graph -> Graph.Graph
-     *)
-
-
 (** keep_links g h v, finds all the nodes connected to v in g - h*)
 val keep_links: Graph.graph -> Graph.verticies -> Graph.vertex -> Graph.verticies
