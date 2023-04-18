3cbb4decb141ba8c08505a364c17123ce037f37a
gwentinho <qschroed@gmail.com>
2023-04-18 17:37:34 +0200

    implemented new test format for proof fingerprints

--- tests/rules_test/rules_test.ml
+++ tests/rules_test/rules_test.ml
@@ -2,15 +2,25 @@ open Gscore
 exception READERROR of string
 
 
+
 (*2 values in each file, expected and start*)
 let path = "./fingerprints.json"
 
 let proofs = Parsegraph.read_file_as_fingerprints path
 (*Graph from example 4.16 in "An analytic proof system on graphs"*)
 
-let rec verify_proofs (proofs: Fingerprint.proof list) = match proofs with
-  | [] -> ()
-  | proof :: rest ->
-    let name = proof.id in
-    let _ = Fingerprint.verify proof in
-    verify_proofs rest (*TODO change tests to work here*)
+let proofs_arr = Array.of_list proofs
+
+let len = Array.length proofs_arr
+
+
+let () = assert (len = 4)
+
+
+let () = Pp_new.show_tree (Option.get (Fingerprint.verify (proofs_arr.(3))))
+let () = Pp_new.show_graph_as_tree (fst proofs_arr.(3).expected) (snd proofs_arr.(3).expected)
+
+let%test "test_ai" = None = Fingerprint.verify (proofs_arr.(0))
+let%test "test_pp_one_step" = None = Fingerprint.verify (proofs_arr.(1))
+let%test "test_pp_verif" = None = Fingerprint.verify (proofs_arr.(2))
+let%test "test_sw" = None = Fingerprint.verify (proofs_arr.(3))
