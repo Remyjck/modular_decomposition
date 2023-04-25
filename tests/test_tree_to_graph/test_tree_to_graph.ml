d45f98893fc858837c0c4e2c9473aeb78a47090b
Gwentinho <qschroed@gmail.com>
2023-04-25 23:13:30 +0200

    new tests

--- tests/test_tree_to_graph/test_tree_to_graph.ml
+++ tests/test_tree_to_graph/test_tree_to_graph.ml
@@ -3,7 +3,21 @@ open Base
 
 let graph = Parsegraph.read_file_as_graph "./graph.json"
 let tree = Condense.tree_from_graph graph |> Caml.Option.get
-let graph2 = Tree.tree_to_graph tree
+let graphT = Tree.tree_to_graph tree
+let%test "graph1_eq_nodes" = Graph.VSet.equal graph.nodes graphT.nodes
+let%test "graph1_eq_maps" = Map.equal Graph.VSet.equal graph.edges graphT.edges
+let graph2 = Parsegraph.read_file_as_graph "./graph2.json"
+let tree = Condense.tree_from_graph graph |> Caml.Option.get (*these fail now*)
+let graphT2 = Tree.tree_to_graph tree
+let%test "graph2_eq_nodes" = Graph.VSet.equal graph2.nodes graphT2.nodes
 
-let%test _ = Graph.VSet.equal graph.nodes graph2.nodes
-let%test _ = Map.equal Graph.VSet.equal graph.edges graph2.edges
+let%test "graph2_eq_maps" =
+  Map.equal Graph.VSet.equal graph2.edges graphT2.edges
+
+let graph3 = Parsegraph.read_file_as_graph "./graph3.json"
+let tree = Condense.tree_from_graph graph |> Caml.Option.get
+let graphT3 = Tree.tree_to_graph tree
+let%test "graph3_eq_nodes" = Graph.VSet.equal graph3.nodes graphT3.nodes
+
+let%test "graph3_eq_maps" =
+  Map.equal Graph.VSet.equal graph3.edges graphT3.edges
