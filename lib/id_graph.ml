8d61e14329cf33893f55f774f9340bfc8d3204f1
Gwentinho <qschroed@gmail.com>
2023-04-30 17:21:28 +0200

    updated iso alg to give an explicit iso and fixes

--- lib/id_graph.ml
+++ lib/id_graph.ml
@@ -8,8 +8,7 @@ type id_graph = { nodes : int list; edges : (int * int) list }
 let isEdge (a, b) idg =
   let { nodes = _; edges } = idg in
   (*use the or statement because we are working with undirected edges*)
-  List.find edges ~f:(fun (c, d) -> (a = c && b = d) || (a = d && b = c))
-  <> None
+  List.exists edges ~f:(fun (c, d) -> (a = c && b = d) || (a = d && b = c))
 
 let length idg =
   let { nodes; edges = _ } = idg in
@@ -46,12 +45,25 @@ let genInitialPerm idg1 idg2 dim1 dim2 =
   Mat.init dim1 dim2 (fun i j ->
       if degArr2.(j - 1) >= degArr1.(i - 1) then 1.0 else 0.0)
 
+let permToAssoc idg1 idg2 perm =
+  let nodes1 = Array.of_list idg1.nodes in
+  let nodes2 = Array.of_list idg2.nodes in
+  let perm_assoc =
+    List.concat_mapi (Mat.to_list perm) ~f:(fun j l ->
+        List.mapi l ~f:(fun i e -> (i, j, e)))
+  in
+  let filtered = List.filter perm_assoc ~f:(fun (_, _, e) -> e = 1.0) in
+  let filtered = List.map filtered ~f:(fun (i, j, _) -> (i, j)) in
+  let filtered =
+    List.map filtered ~f:(fun (i, j) -> (nodes1.(i), nodes2.(j)))
+  in
+  Map.of_alist_exn (module Int) filtered
+
 let setImmut a i v =
   let cpy = Array.copy a in
   let () = cpy.(i) <- v in
   cpy
 
-(*TODO test (<=) *)
 let isIso perm matA matB = matA <= mulT perm (mul perm matB)
 
 (*TODO implement prune*)
@@ -61,45 +73,62 @@ let refine perm = perm
   https://adriann.github.io/Ullman%20subgraph%20isomorphism.html
 *)
 
-(*Finds some isomorphism between matA and matB returns a bool on success*)
-(*nodes(A) * nodes(B) matrix is perm*)
+(*Finds some isomorphism between matA and matB returns a Some iso on success*)
 let ullmann_find perm matA matB =
   let lastRow = to_int (Mat.dim1 matA) in
   let lastCol = to_int (Mat.dim1 matB) in
   let rec aux freeVert perm currentRow currentColumn =
     let perm = refine perm in
-    if currentRow = lastRow && isIso perm matA matB then true
-    else if currentColumn = lastCol then false
+    if currentRow = lastRow && isIso perm matA matB then Some perm
+    else if currentColumn = lastCol then None
     else if freeVert.(currentColumn) then
       aux freeVert perm currentRow (currentColumn + 1)
     else
+      let perm = Mat.copy perm in
+      let () =
+        Mat.replace_alli perm (fun i j a ->
+            match (i, j, a) with
+            | i, j, _ when i = currentRow + 1 && j = currentColumn + 1 -> 1.0
+            | i, _, _ when i = currentRow + 1 -> 0.0
+            | _, _, a -> a)
+      in
       (*We are now in the for loop*)
       (*Mark c as used*)
+      (*TODO why is this working*)
       let freeVert = setImmut freeVert currentColumn true in
       aux freeVert perm (currentRow + 1) currentColumn
   in
   aux (Array.create ~len:lastCol false) perm 0 0
 
-let is_sub_iso idg1 idg2 =
+let find_sub_iso idg1 idg2 =
+  let ( >>= ) = Option.( >>= ) in
+  let return = Option.return in
   let l1 = length idg1 in
   let l2 = length idg2 in
-  if l1 = 0 then true
-  else if (*We want empty graphs to be trivial subgraphs*)
-          l1 > l2 then false
-  else if length_edges idg1 = 0 then true
+  if (*We want empty graphs to be trivial subgraphs*)
+     l1 > l2 then None
   else
     let module Dim1 = (val of_int_dyn l1 : SIZE) in
     let module Dim2 = (val of_int_dyn l2 : SIZE) in
     let m0 = genInitialPerm idg1 idg2 Dim1.value Dim2.value in
     let adj1 = adj_mat idg1 Dim1.value in
     let adj2 = adj_mat idg2 Dim2.value in
-    ullmann_find m0 adj1 adj2
+    ullmann_find m0 adj1 adj2 >>= fun perm ->
+    return (permToAssoc idg1 idg2 perm)
 
-let is_iso idg1 idg2 = is_sub_iso idg1 idg2 && is_sub_iso idg2 idg1
-let ( >>= ) = List.( >>= )
+let is_iso idg1 idg2 =
+  match (find_sub_iso idg1 idg2, find_sub_iso idg2 idg1) with
+  | Some _, Some _ -> true
+  | _ -> false
+
+let find_iso idg1 idg2 =
+  match (find_sub_iso idg1 idg2, find_sub_iso idg2 idg1) with
+  | Some m1, Some _ -> Some m1
+  | _ -> None
 
 (*fills out all possible edges of the graph TODO, not very optimal but hey*)
 let completetion_graph g =
+  let ( >>= ) = List.( >>= ) in
   let { nodes; edges = _ } = g in
   let edges =
     nodes >>= fun x ->
--- lib/id_graph.mli
+++ lib/id_graph.mli
@@ -1,8 +1,22 @@
 type id_graph = { nodes : int list; edges : (int * int) list }
 
+val isEdge : int * int -> id_graph -> bool
 val length : id_graph -> int
-val is_sub_iso : id_graph -> id_graph -> bool
+val length_edges : id_graph -> int
+
+val find_sub_iso :
+  id_graph ->
+  id_graph ->
+  (int, int, Base.Int.comparator_witness) Base.Map.t option
+
 val is_iso : id_graph -> id_graph -> bool
+
+val find_iso :
+  id_graph ->
+  id_graph ->
+  (int, int, Base.Int.comparator_witness) Base.Map.t option
+
 val completetion_graph : id_graph -> id_graph
+val edge_diff : id_graph -> id_graph -> (int * int) list
 val id_graph_complement : id_graph -> id_graph
 val is_dual : id_graph -> id_graph -> bool
