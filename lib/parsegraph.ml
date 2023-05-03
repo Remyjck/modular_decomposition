79248360ac25ec42c9d3e80fc9602e5d542230ac
Gwentinho <qschroed@gmail.com>
2023-05-03 22:33:47 +0200

    updated parsing for new formats and + tree reading

--- lib/parsegraph.ml
+++ lib/parsegraph.ml
@@ -2,17 +2,16 @@ open Graph
 open Base
 open Yojson.Basic.Util
 
-let to_vertex js_obj =
+let to_node_and_id js_obj =
   let id = js_obj |> member "id" |> to_int in
   let label = js_obj |> member "label" |> to_string in
-  let polarisation = js_obj |> member "polarisation" |> to_bool in
-  let atom = Atom { label; pol = polarisation } in
-  { connective = atom; id }
+  let pol = js_obj |> member "polarisation" |> to_bool in
+  let atom = Atom { label; pol } in
+  (atom, id)
 
-let to_nodes js_obj =
+let to_nodes_and_ids js_obj =
   let json_list = to_list js_obj in
-  let vertex_list = List.map json_list ~f:to_vertex in
-  Set.of_list (module Vertex) vertex_list
+  Base.List.unzip (List.map json_list ~f:to_node_and_id)
 
 let to_id_tuple js_obj =
   let src = js_obj |> member "source" |> to_int in
@@ -21,59 +20,69 @@ let to_id_tuple js_obj =
 
 let to_id_list js_obj = to_list js_obj |> List.map ~f:to_id_tuple
 
-let to_assoc_list id_list nodes =
-  let map = id_map nodes in
+(** [id_map vset]: returns a map from the [id]s of the elements of [vset] to
+    the elements themselves *)
+let id_map (vs : node list) ids =
+  List.fold (List.zip_exn vs ids)
+    ~init:(Map.empty (module Int))
+    ~f:(fun accum (node, id) -> Map.add_exn accum ~key:id ~data:node)
+
+let to_assoc_list id_list nodes ids =
+  let map = id_map nodes ids in
   List.map id_list ~f:(fun (src, dest) ->
       (Map.find_exn map src, Map.find_exn map dest))
 
-let to_edge_maps nodes js_obj =
+let to_edge_maps nodes ids js_obj =
   let id_list = to_id_list js_obj in
-  let vertex_assoc = to_assoc_list id_list nodes in
+  let vertex_assoc = to_assoc_list id_list nodes ids in
   Graph.edge_maps vertex_assoc
 
 let parse js_obj =
-  let nodes = js_obj |> member "nodes" |> to_nodes in
-  let edges = js_obj |> member "edges" |> to_edge_maps nodes in
+  let nodes_json = js_obj |> member "nodes" in
+  let nodes, ids = to_nodes_and_ids nodes_json in
+  let edges = js_obj |> member "edges" |> to_edge_maps nodes ids in
+  let nodes = Set.of_list (module Node) nodes in
   { nodes; edges }
 
 let make_tree_node node successors : Tree.tree =
   let connective_label = node |> member "connective" |> to_string in
-  let id = node |> member "id" |> to_int in
-  let connective =
-    match connective_label with
-    | "prime" ->
-        let graph = node |> member "graph" in
-        let id_nodes = List.map (graph |> to_list) ~f:to_int in
-        let id_edges = graph |> to_id_list in
-        let idg : Id_graph.id_graph = { nodes = id_nodes; edges = id_edges } in
-        Tree.Prime (idg, successors)
-    | "par" -> Par successors
-    | "tensor" -> Tensor successors
-    | "atom" ->
-        Atom { label = Int.to_string id; pol = true }
-        (*TODO could lead to edge cases*)
-    | _ -> failwith "Tried to serialize malformed tree"
-  in
-  { id; connective }
+  match connective_label with
+  | "prime" ->
+      let graph = node |> member "graph" in
+      let nodes = List.map (graph |> member "nodes" |> to_list) ~f:to_int in
+      let edges = graph |> member "edges" |> to_id_list in
+      let idg : Id_graph.id_graph = { nodes; edges } in
+      Tree.Prime (idg, successors)
+  | "par" -> Par successors
+  | "tensor" -> Tensor successors
+  | "atom" ->
+      let label = node |> member "label" |> to_string in
+      let pol = node |> member "polarisation" |> to_bool in
+      Atom { label; pol }
+  | _ -> failwith "Tried to serialize malformed tree"
 
 let rec parse_tree js_obj =
-  let successors = js_obj |> member "successors" |> to_list in
-  let successors = List.map successors ~f:parse_tree in
-  let node = js_obj |> member "node" in
-  make_tree_node node successors
+  let ( = ) = Poly.( = ) in
+  (*check if object is an empty dictionary*)
+  if js_obj = `Assoc [] then Tree.empty_tree ()
+  else
+    let successors =
+      match js_obj |> member "successors" with `Null -> [] | s -> to_list s
+    in
+    let successors = List.map successors ~f:parse_tree in
+    make_tree_node js_obj successors
 
-let from_vertex vertex =
-  let id = `Int vertex.id in
+let from_node node =
   let label, pol =
-    match vertex.connective with
+    match node with
     | Atom atom -> (`String atom.label, `Bool atom.pol)
     | _ -> failwith "Tried to serialize non-atomic graph"
   in
-  `Assoc [ ("id", id); ("label", label); ("polarisation", pol) ]
+  `Assoc [ ("label", label); ("polarisation", pol) ]
 
-let from_nodes vset =
-  let node_list = Set.elements vset in
-  let json_list = List.map node_list ~f:from_vertex in
+let from_nodes nset =
+  let node_list = Set.elements nset in
+  let json_list = List.map node_list ~f:from_node in
   `List json_list
 
 let from_id_tuple (id1, id2) =
@@ -83,7 +92,9 @@ let from_id_tuple (id1, id2) =
 
 let from_edges edge_map =
   let edge_list = edge_tuple_list edge_map in
-  let id_list = List.map edge_list ~f:(fun (v1, v2) -> (v1.id, v2.id)) in
+  let id_list =
+    List.map edge_list ~f:(fun (v1, v2) -> (hash_node v1, hash_node v2))
+  in
   let json_list = List.map id_list ~f:from_id_tuple in
   json_list
 
@@ -94,7 +105,6 @@ let serialize_graph graph =
 
 let serialize_id_graph (id_graph : Id_graph.id_graph) =
   let nodesMapping = List.mapi id_graph.nodes ~f:(fun i v -> (v, i + 1)) in
-  (*use hashmaps instead later TODO*)
   let reindexedEdges =
     List.map id_graph.edges ~f:(fun (s, t) ->
         ( snd @@ Caml.Option.get
@@ -105,13 +115,6 @@ let serialize_id_graph (id_graph : Id_graph.id_graph) =
   let nodeCount = `Int (List.length nodesMapping) in
   let edges = `List (List.map reindexedEdges ~f:from_id_tuple) in
   `Assoc [ ("nodeCount", nodeCount); ("edges", edges) ]
-
-let from_connective connective =
-  match connective with
-  | Tree.Atom _ -> (`String "atom", None)
-  | Tree.Tensor _ -> (`String "tensor", None)
-  | Tree.Par _ -> (`String "par", None)
-  | Tree.Prime (id_graph, _) -> (`String "prime", Some id_graph)
 
 let from_id_graph (id_graph : Id_graph.id_graph) =
   let nodes = List.map id_graph.nodes ~f:(fun n -> `Int n) in
@@ -124,16 +127,29 @@ let from_id_graph (id_graph : Id_graph.id_graph) =
   `Assoc [ ("nodes", nodes_json); ("edges", edges_json) ]
 
 let rec serialize_tree (tree : Tree.tree) : Yojson.Basic.t =
-  let id = `Int tree.id in
-  let connective, id_graph = from_connective tree.connective in
-  let node_base = [ ("connective", connective); ("id", id) ] in
-  let node =
-    match id_graph with
-    | None -> `Assoc node_base
-    | Some graph -> `Assoc (("graph", from_id_graph graph) :: node_base)
-  in
-  let successors = List.map (Tree.successors tree) ~f:serialize_tree in
-  `Assoc [ ("node", node); ("successors", `List successors) ]
+  match tree with
+  | Tree.Atom a ->
+      let node_base = [ ("connective", `String "atom") ] in
+      let label = ("label", `String a.label) in
+      let pol = ("polarisation", `Bool a.pol) in
+      `Assoc (label :: pol :: node_base)
+  | Tree.Tensor succ ->
+      let node_base = [ ("connective", `String "tensor") ] in
+      let successors = List.map succ ~f:serialize_tree in
+      `Assoc (("successors", `List successors) :: node_base)
+  | Tree.Par succ ->
+      let node_base = [ ("connective", `String "par") ] in
+      let successors = List.map succ ~f:serialize_tree in
+      `Assoc (("successors", `List successors) :: node_base)
+  | Tree.Prime (id_graph, succ) ->
+      let connective = `String "prime" in
+      let node_base = [ ("connective", connective) ] in
+
+      let successors = List.map succ ~f:serialize_tree in
+      `Assoc
+        (("successors", `List successors)
+        :: ("graph", from_id_graph id_graph)
+        :: node_base)
 
 let read_file_as_graph filepath =
   let s = Stdio.In_channel.read_all filepath in
@@ -181,11 +197,10 @@ let to_step js_obj =
   | _ -> failwith "Tried to serialize malformed proof"
 
 let parse_fingerprint js_obj : Fingerprint.proof =
-  let id = js_obj |> member "id" |> to_string in
-  let initial = js_obj |> member "initial" |> parse in
-  let expected = js_obj |> member "expected" |> parse in
+  let initial = js_obj |> member "initial" |> parse_tree in
+  let expected = js_obj |> member "expected" |> parse_tree in
   let steps = List.map ~f:to_step (js_obj |> member "steps" |> to_list) in
-  { id; initial; expected; steps }
+  { initial; expected; steps }
 
 let read_file_as_fingerprints filepath =
   let s = Stdio.In_channel.read_all filepath in
--- lib/parsegraph.mli
+++ lib/parsegraph.mli
@@ -1,41 +1,5 @@
-val to_vertex : Yojson.Basic.t -> Graph.vertex
-
-val to_nodes :
-  Yojson.Basic.t -> (Graph.vertex, Graph.Vertex.comparator_witness) Base.Set.t
-
-val to_id_tuple : Yojson.Basic.t -> int * int
-val to_id_list : Yojson.Basic.t -> (int * int) list
-
-val to_assoc_list :
-  (int * int) list -> Graph.verticies -> (Graph.vertex * Graph.vertex) list
-
-val to_edge_maps : Graph.verticies -> Yojson.Basic.t -> Graph.edges
-val parse : Yojson.Basic.t -> Graph.graph
-
-val from_vertex :
-  Graph.vertex ->
-  [> `Assoc of
-     (string * [> `Bool of bool | `Int of int | `String of string ]) list ]
-
-val from_nodes :
-  (Graph.vertex, 'a) Base.Set.t ->
-  [> `List of
-     [> `Assoc of
-        (string * [> `Bool of bool | `Int of int | `String of string ]) list ]
-     list ]
-
-val serialize_graph :
-  Graph.graph ->
-  [> `Assoc of
-     (string
-     * [> `List of
-          [> `Assoc of
-             (string * [> `Bool of bool | `Int of int | `String of string ])
-             list ]
-          list ])
-     list ]
-
 val serialize_tree : Tree.tree -> Yojson.Basic.t
+val serialize_graph : Graph.graph -> Yojson.Basic.t
 val read_file_as_graph : string -> Graph.graph
 val read_file_as_graphs : string -> Graph.graph list
 val read_file_as_id_graph : string -> Id_graph.id_graph
