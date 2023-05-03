178c95d053d15eb6b8d1d83d1f89cc13fb79338e
Gwentinho <qschroed@gmail.com>
2023-05-03 22:32:42 +0200

    renaming

--- lib/graph_module.ml
+++ lib/graph_module.ml
@@ -1,23 +1,21 @@
 open Base
 open Graph
 
-type context = { graph : graph; connnected : verticies }
-
 let share_module graph vi vj =
   let si = find_or_empty graph.edges vi |> Fn.flip Set.remove vj in
   let sj = find_or_empty graph.edges vj |> Fn.flip Set.remove vi in
-  VSet.equal si sj
+  NSet.equal si sj
 
-let keep_links g (h : verticies) v : verticies =
+let keep_links g (h : nodes) v : nodes =
   let new_h = Set.remove h v in
   match Map.find (graph_difference g new_h).edges v with
-  | None -> Set.empty (module Vertex)
+  | None -> Set.empty (module Node)
   | Some vset -> vset
 
-let is_module g (h : verticies) =
+let is_module g (h : nodes) =
   let connected =
     match Set.choose h with
-    | None -> Set.empty (module Vertex)
+    | None -> Set.empty (module Node)
     | Some v -> keep_links g h v
   in
   Set.for_all h ~f:(fun v ->
--- lib/graph_module.mli
+++ lib/graph_module.mli
@@ -1,8 +1,5 @@
-type context = { graph : Graph.graph; connnected : Graph.verticies }
+val share_module : Graph.graph -> Graph.node -> Graph.node -> bool
+val is_module : Graph.graph -> Graph.nodes -> bool
 
-val share_module : Graph.graph -> Graph.vertex -> Graph.vertex -> bool
-val is_module : Graph.graph -> Graph.verticies -> bool
-
-val keep_links :
-  Graph.graph -> Graph.verticies -> Graph.vertex -> Graph.verticies
+val keep_links : Graph.graph -> Graph.nodes -> Graph.node -> Graph.nodes
 (** keep_links g h v, finds all the nodes connected to v in g - h*)
