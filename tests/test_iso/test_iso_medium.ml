84914920c68d1569a20f63d288b26c620a89a136
Gwentinho <qschroed@gmail.com>
2023-05-14 17:16:14 +0200

    added in old tests

---
+++ tests/test_iso/test_iso_medium.ml
@@ -0,0 +1,24 @@
+open Logic.Idgraph
+
+let g1 : id_graph =
+  { nodes = [ 1; 5; 10; 27 ]; edges = [ (1, 5); (5, 10); (10, 27) ] }
+
+let g2 : id_graph =
+  { nodes = [ 2; 3; 10; 27 ]; edges = [ (2, 3); (3, 10); (10, 27) ] }
+
+let g3 : id_graph =
+  { nodes = [ 3; 2; 10; 27 ]; edges = [ (3, 2); (3, 10); (10, 27) ] }
+
+let g4 : id_graph = { nodes = [ 2; 3; 10 ]; edges = [ (2, 3); (3, 10) ] }
+let%test "sub_iso_renaming_left" = is_sub_iso g1 g2
+let%test "sub_iso_renaming_right" = is_sub_iso g2 g1
+let%test "sub_iso_reordering_left" = is_sub_iso g3 g2
+let%test "sub_iso_reordering_right" = is_sub_iso g2 g3
+let%test "sub_iso_reordering2_left" = is_sub_iso g3 g1
+let%test "sub_iso_reordering2_right" = is_sub_iso g1 g3
+let%test "sub_iso_shortened_left1" = is_sub_iso g4 g1
+let%test "sub_iso_shortened_left2" = is_sub_iso g4 g2
+let%test "sub_iso_shortened_left3" = is_sub_iso g4 g3
+let%test "sub_iso_shortened_not_right1" = not (is_sub_iso g1 g4)
+let%test "sub_iso_shortened_not_right2" = not (is_sub_iso g2 g4)
+let%test "sub_iso_shortened_not_right3" = not (is_sub_iso g3 g4)
---
+++ tests/test_rules/test_rules.ml
@@ -0,0 +1,19 @@
+open Logic
+
+exception READERROR of string
+
+(*2 values in each file, expected and start*)
+let path = "./fingerprints.json"
+let proofs = Parseproofs.read_file_as_fingerprints path
+let proofs_arr = Array.of_list proofs
+let len = Array.length proofs_arr
+let () = assert (len = 9)
+let%test "test_ai1" = None = Fingerprint.verify proofs_arr.(0)
+let%test "test_ai2" = None <> Fingerprint.verify proofs_arr.(1)
+let%test "test_ai3" = None = Fingerprint.verify proofs_arr.(2)
+let%test "test_ai4" = None = Fingerprint.verify proofs_arr.(3)
+let%test "test_ai5" = None <> Fingerprint.verify proofs_arr.(4)
+let%test "test_ai6" = None <> Fingerprint.verify proofs_arr.(5)
+let%test "test_pp_one_step" = None = Fingerprint.verify proofs_arr.(6)
+let%test "test_pp_verif" = None = Fingerprint.verify proofs_arr.(7)
+let%test "test_sw" = None = Fingerprint.verify proofs_arr.(8)
